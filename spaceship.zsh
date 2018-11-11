89728ed70df67c8bb9c0bcf612aa805086db1524
Tim Condit <tcondit@gmail.com>
2018-11-11 09:36:18 -0800

    disable prompt sections I don't use

--- spaceship.zsh
+++ spaceship.zsh
@@ -45,26 +45,26 @@ if [ -z "$SPACESHIP_PROMPT_ORDER" ]; then
     dir           # Current directory section
     host          # Hostname section
     git           # Git section (git_branch + git_status)
-    hg            # Mercurial section (hg_branch  + hg_status)
+#    hg            # Mercurial section (hg_branch  + hg_status)
     package       # Package version
-    node          # Node.js section
+#    node          # Node.js section
     ruby          # Ruby section
     elm           # Elm section
     elixir        # Elixir section
-    xcode         # Xcode section
-    swift         # Swift section
+#    xcode         # Xcode section
+#    swift         # Swift section
     golang        # Go section
-    php           # PHP section
-    rust          # Rust section
-    haskell       # Haskell Stack section
-    julia         # Julia section
+#    php           # PHP section
+#    rust          # Rust section
+#    haskell       # Haskell Stack section
+#    julia         # Julia section
     docker        # Docker section
     aws           # Amazon Web Services section
     venv          # virtualenv section
-    conda         # conda virtualenv section
+#    conda         # conda virtualenv section
     pyenv         # Pyenv section
-    dotnet        # .NET section
-    ember         # Ember.js section
+#    dotnet        # .NET section
+#    ember         # Ember.js section
     kubecontext   # Kubectl context section
     exec_time     # Execution time
     line_sep      # Line break
